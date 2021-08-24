de299dfade78f9aea3fd32e9e07f5bb7bd1499bf
shamagir <shamagir@gmail.com>
2021-08-24 12:27:50 +0300

    feat: element-search

---
+++ element-search/menu/task.js
@@ -0,0 +1,26 @@
+let menuItems = Array.from(document.querySelectorAll('.menu__item')),
+  closestSubHref;
+
+menuItems.forEach(function(item, i, arr){
+  checkSub( item );
+});
+
+function checkSub(element) {
+  if(element.querySelector('.menu_sub')) {
+    closestSubHref = element.querySelector('.menu__link');
+    closestSubHref.onclick = function(){
+      subSwitcher(element);
+      return false;
+    }
+  }
+  return;
+}
+
+function subSwitcher(element) {
+  if(element.querySelector('.menu_active')){
+    element.querySelector('.menu_active').classList.remove('menu_active');
+  } else {
+    element.querySelector('.menu_sub').classList.add('menu_active');
+  }
+  return;
+}
---
+++ element-search/popups/task.js
@@ -0,0 +1,22 @@
+document.getElementById('modal_main').classList.add('modal_active');
+
+let modalCloseButtons = document.querySelectorAll('.modal__close'),
+  modalSuccess = document.getElementById('modal_success'),
+  currentActiveModal;
+const modalCloseButtonsLength = modalCloseButtons.length;
+
+for(i = 0; i < modalCloseButtonsLength; i += 1){
+  modalCloseButtons[i].onclick = function() {
+    checkClasses(this);
+  }
+}
+
+function checkClasses(element) {
+  currentActiveModal = element.closest('.modal');
+  if(element.classList.contains('show-success')){
+    currentActiveModal.classList.remove('modal_active');
+    modalSuccess.classList.add('modal_active');
+  } else {
+    currentActiveModal.classList.remove('modal_active');
+  }
+}
