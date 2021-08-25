f557dd83648b2863ff2a4b0353361d5767e40276
shamagir <shamagir@gmail.com>
2021-08-25 13:06:29 +0300

    fix: toggle active sub menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -9,18 +9,9 @@ function checkSub(element) {
   if (element.querySelector('.menu_sub')) {
     closestSubHref = element.querySelector('.menu__link');
     closestSubHref.onclick = function () {
-      subSwitcher(element);
+      element.querySelector('.menu_sub').classList.toggle('menu_active');
       return false;
     };
-  }
-  return;
-}
-
-function subSwitcher(element) {
-  if (element.querySelector('.menu_active')) {
-    element.querySelector('.menu_active').classList.remove('menu_active');
-  } else {
-    element.querySelector('.menu_sub').classList.add('menu_active');
   }
   return;
 }
