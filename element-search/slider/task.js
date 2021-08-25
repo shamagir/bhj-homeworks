bde269d6f00d15547c1192aad174b294a9b305b5
shamagir <shamagir@gmail.com>
2021-08-25 12:56:00 +0300

    fix: toggle active slide

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -5,7 +5,7 @@ let sliderItems = Array.from(document.querySelectorAll('.slider__item')),
 
 sliderArrows.forEach(function (item) {
   item.onclick = function () {
-    setSliderUnactive();
+    toggleActive();
     sliderIndex = this.classList.contains('slider__arrow_next')
       ? sliderIndex + 1
       : sliderIndex - 1;
@@ -15,23 +15,19 @@ sliderArrows.forEach(function (item) {
 
 sliderDots.forEach(function (item) {
   item.onclick = function () {
-    setSliderUnactive();
+    toggleActive();
     sliderIndex = sliderDots.indexOf(this);
-    setSliderActive();
+    toggleActive();
   };
 });
 
 function checkSliderIndex() {
   sliderIndex = sliderIndex === sliderItems.length ? 0 : sliderIndex;
   sliderIndex = sliderIndex < 0 ? sliderItems.length - 1 : sliderIndex;
-  setSliderActive();
+  toggleActive();
 }
 
-function setSliderActive() {
-  sliderItems[sliderIndex].classList.add('slider__item_active');
-}
-
-function setSliderUnactive() {
-  sliderItems[sliderIndex].classList.remove('slider__item_active');
+function toggleActive() {
+  sliderItems[sliderIndex].classList.toggle('slider__item_active');
 }
 
