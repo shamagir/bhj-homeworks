76cb28d1159afb8a7e2b107bcb8013aaae22b781
shamagir <shamagir@gmail.com>
2021-09-05 16:35:30 +0300

    feat: chat

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -2,9 +2,71 @@
 
 const chatWidget    = document.querySelector( '.chat-widget' ); 
 const chatActive    = 'chat-widget_active';
-const clientMessage = 'message_client';
-
+const inputField    = document.querySelector( '.chat-widget__input' );
+const container     = document.querySelector( '.chat-widget__messages-container' );
 const messages      = document.querySelector( '.chat-widget__messages' );
 const chatActivator = document.querySelector( '.chat-widget__side' );
 
-chatActivator.addEventListener('click', () => chatWidget.classList.add(chatActive));
+let flag;
+let interval;
+let intervalTime = 4000;
+let date         = new Date(); 
+let responses    = [
+	'По пятницам не подаём!', 
+	'От дохлого осла уши!',
+	'Уйди, старушка, я в печали!',
+	'Кому и кобыла - невеста',
+	'Из Пскову мы! На собачку говорящую посмотреть пришли!'
+];
+
+chatActivator.addEventListener( 'click', () => {
+	chatWidget.classList.add( chatActive )
+	interval = setInterval(() => robotMessage(), intervalTime); 
+});
+
+function checkWhitespace() {
+	for( let i=0; i<inputField.value.length; i++ ){
+		flag = ( inputField.value.charAt(i) !== ' ' ) ? 1 : 0; 
+	}
+	return flag;
+}
+
+function robotMessage() {
+	let answerIndex = Math.floor( Math.random() * responses.length );
+	let answerText  = responses[ answerIndex ];
+
+	messages.innerHTML += `
+		<div class="message">
+			<div class="message__time">${ date.getHours() }:${ date.getMinutes() }</div>
+				<div class="message__text">
+					${ answerText }
+				</div>
+			</div>
+		</div>
+	`;
+	container.scrollTop = container.scrollHeight;
+}
+
+const chatKeydown = ( event ) => {
+	clearInterval( interval );
+	if( event.key === 'Enter' ){
+		flag = checkWhitespace();
+		if( inputField.value.length > 0 && flag === 1 ) {
+			messages.innerHTML += `
+				<div class="message message_client">
+					<div class="message__time">${ date.getHours()}:${date.getMinutes() }</div>
+						<div class="message__text">
+							${ inputField.value }
+						</div>
+					</div>
+				</div>
+			`;
+			inputField.value = '';
+			robotMessage();
+			container.scrollTop = container.scrollHeight;
+			interval = setInterval(() => robotMessage(), intervalTime );
+		}
+	}
+}
+
+inputField.addEventListener( 'keydown', chatKeydown );
